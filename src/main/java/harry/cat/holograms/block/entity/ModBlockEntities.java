9b26b3ab3790e29bcb6b76b0d147716b8721025a
braden <braden.colbert6550@gmail.com>
2025-07-20 14:43:22 -0700

    Attempted Implementation of Block Entity round three

--- src/main/java/harry/cat/holograms/block/entity/ModBlockEntities.java
+++ src/main/java/harry/cat/holograms/block/entity/ModBlockEntities.java
@@ -4,6 +4,8 @@ import harry.cat.holograms.Holograms;
 import harry.cat.holograms.block.ModBlocks;
 import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityType;
 import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
+import net.minecraft.block.Block;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -11,16 +13,19 @@ import net.minecraft.util.Identifier;
 import harry.cat.holograms.block.ModBlocks;
 import harry.cat.holograms.block.entity.custom.HoloTableBlockEntity;
 
+
 import static org.spongepowered.asm.mixin.injection.struct.InjectionInfo.register;
 
 public class ModBlockEntities {
-    public static final BlockEntityType<HoloTableBlockEntity> HOLO_TABLE = register(
-            Registries.BLOCK_ENTITY_TYPE,
-            Identifier.of("holograms", "holo_table"),
-            FabricBlockEntityType.Builder.create(HoloTableBlockEntity::new, ModBlocks.HOLO_TABLE).build()
-    );
-    public static void initialize() {
-        Holograms.LOGGER.info("Registering block entities for " + Holograms.MOD_ID);
+    public static final BlockEntityType<HoloTableBlockEntity> HOLO_TABLE =
+            register("counter", HoloTableBlockEntity::new, ModBlocks.HOLO_TABLE);
+
+    private static <T extends BlockEntity> BlockEntityType<T> register(
+            String name,
+            FabricBlockEntityTypeBuilder.Factory<? extends T> entityFactory,
+            Block... blocks
+    ) {
+        Identifier id = Identifier.of(Holograms.MOD_ID, name);
+        return Registry.register(Registries.BLOCK_ENTITY_TYPE, id, FabricBlockEntityTypeBuilder.<T>create(entityFactory, blocks).build());
     }
 }
-
