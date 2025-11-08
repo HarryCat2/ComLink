2d0ddafcb9f67dcf82c2af328e591b71f67bc162
Harry Cat <braden.colbert6550@gmail.com>
2025-11-08 03:59:43 -0800

    removed the "_" from mobeffects as Delta gets "irked" by the mere existence of them in category names

--- src/main/java/harry/cat/holograms/mobeffects/CarboniteFrozen.java
+++ src/main/java/harry/cat/holograms/mobeffects/CarboniteFrozen.java
@@ -1,4 +1,4 @@
-package harry.cat.holograms.mob_effects;
+package harry.cat.holograms.mobeffects;
 
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffect;
@@ -6,23 +6,22 @@ import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 
-public class CarboniteBlindness extends StatusEffect {
-    public CarboniteBlindness() {
-        super(StatusEffectCategory.HARMFUL, 0xe9b8b3);
+public class CarboniteFrozen extends StatusEffect {
+    public CarboniteFrozen() {
+    super(StatusEffectCategory.NEUTRAL, 0xe9b8b3);
     }
 
     @Override
-    public boolean canApplyUpdateEffect(int duration, int amplifier){
+    public boolean canApplyUpdateEffect(int duration, int amplifier) {
         return true;
     }
 
     @Override
     public boolean applyUpdateEffect(ServerWorld world, LivingEntity entity, int amplifier) {
         if (entity instanceof PlayerEntity) {
-            // ADD EFFECTS LOGIC HERE
-            //((PlayerEntity) entity)
-
+            ((PlayerEntity) entity).addExperience(1 << amplifier);
         }
         return super.applyUpdateEffect(world, entity, amplifier);
     }
+
 }
